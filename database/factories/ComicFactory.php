bd67aeaa5e931382784ad1fff890436bafe19440
alessiafabbioni <alessiafabbioni@gmail.com>
2024-02-15 15:17:30 +0100

    Factory & Seeder

---
+++ database/factories/ComicFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comic>
+ */
+class ComicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'author' => $this->faker->name,
+            'genre' => $this->faker->word,
+            'publication_year' => $this->faker->year,
+            'description' => $this->faker->paragraph,
+        ];
+    }
+}
