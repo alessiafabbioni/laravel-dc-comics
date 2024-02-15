45f085212fe9f8bf0671b2ef7f8e1d9346afe167
alessiafabbioni <alessiafabbioni@gmail.com>
2024-02-15 15:42:57 +0100

    Add comics to front page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,31 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Comics Archives: {{count($comics)}}</h1>
+    
+       
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Genre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic -> id}}</th>
+                            <td>{{ $comic -> title}}</td>
+                            <td>{{ $comic -> author}}</td>
+                            <td>{{ $comic -> genre}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        
+        
+        
+      
 @endsection
